2ddfc7973b4d1e92e3cff7d2bfdd21d7b034d71c
danielboros78 <danielboros78@gmail.com>
2021-10-12 22:58:34 +0200

    Add files via upload

---
+++ Fajta.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KutyakForm
+{
+  class Fajta
+  {
+    private int id;
+
+    public int Id
+    {
+      get { return id; }
+    }
+
+    private string fajtaNev;
+
+    public string FajtaNev
+    {
+      get { return fajtaNev; }
+    }
+
+    private string eredetiNev;
+
+    public string EredetiNev
+    {
+      get { return eredetiNev; }
+    }
+
+    public Fajta(int id, string fajtaNev, string eredetiNev)
+    {
+      this.id = id;
+      this.fajtaNev = fajtaNev;
+      this.eredetiNev = eredetiNev;
+    }
+  }
+}
---
+++ Nev.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KutyakForm
+{
+  class Nev
+  {
+    private int id;
+
+    public int Id
+    {
+      get { return id; }
+    }
+
+    private string kutyaNev;
+
+    public string KutyaNev
+    {
+      get { return kutyaNev; }
+    }
+
+    public Nev(int id, string kutyaNev)
+    {
+      this.id = id;
+      this.kutyaNev = kutyaNev;
+    }
+  }
+}
